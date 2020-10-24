c8a76d63ef506fd48cadfdc62967394416bb3538
Marc Partensky <marc.partensky@gmail.com>
2020-10-24 12:25:40 +0200

    added rep cmd

--- Openclassrooms/vector.r
+++ Openclassrooms/vector.r
@@ -19,3 +19,5 @@ x = c("bonjour", "hello", "hej")
 rep(c("rouge", "jaune", "bleu"), times=2)
 rep(c("rouge", "jaune", "bleu"), each=2)
 rep(c("rouge", "jaune", "bleu"), times=c(1,4,2))
+nom = paste(rep("ind", 10), 1:10, sep="-")
+nom
