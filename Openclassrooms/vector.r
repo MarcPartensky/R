1bd0a3d32e2b6f755b8b148a861cdc5d21ef0e72
Marc Partensky <marc.partensky@gmail.com>
2020-10-24 12:29:29 +0200

    new cmds

--- Openclassrooms/vector.r
+++ Openclassrooms/vector.r
@@ -21,3 +21,12 @@ rep(c("rouge", "jaune", "bleu"), each=2)
 rep(c("rouge", "jaune", "bleu"), times=c(1,4,2))
 nom = paste(rep("ind", 10), 1:10, sep="-")
 nom
+
+substr("freerider",5,9)
+1>0
+x = c(-1, 0, 2)
+test = x>1
+(1+x^2)*(x>1)
+
+all(x>1)
+any(x>1)
